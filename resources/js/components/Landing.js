20ba12131e6b49667962a2faa487a43d65d7c5fe
Reinhard Kiptoo <kiptoorey@gmail.com>
2020-06-24 10:54:15 +0300

    firstcommit

---
+++ resources/js/components/Landing.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+function Landing() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Example Component</div>
+                        <div className="card-body">
+                            I'm an example component!
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Landing;
+
+if (document.getElementById("landing")) {
+    ReactDOM.render(<Landing />, document.getElementById("landing"));
+}
